a35013cbd650bdfaec6f5a9637ae0171ee0c3af4
Pavitthra Pandurangan <pavitthp@andrew.cmu.edu>
2020-04-26 18:15:21 -0400

    cap

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
+const github_old = require("@actions/github");
 var Github = require('github-api');
 
 var defaultMoodbarMessage = "Hi newcomers! We wanted to check in on everyone - in this thread, feel free to add any of your comments, suggestions, or questions. This community looks forward to looking at your feedback.";
 
 
 function run() {
-    const client = new github.GitHub(core.getInput('repo-token', { required: true }));
+    const client = new github_old.GitHub(core.getInput('repo-token', { required: true }));
     let moodbarMessage = core.getInput('moodbar-message');
     console.log(moodbarMessage);
     const mentors  = core.getInput('mentor-list');
@@ -28,7 +28,7 @@ function run() {
         defaultMoodbarMessage = defaultMoodbarMessage + " Here are the mentors that will be able to answer any questions: " + mentorString; 
     }
 
-    const context = github.context;    
+    const context = github_old.context;    
     const newIssue = client.issues.create({
         ...context.repo,
         title: 'Moodbar for Current Month',
