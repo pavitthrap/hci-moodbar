a42cbbbb4e69c2908db7b8ead874dec8fbbae911
Pavitthra Pandurangan <pavitthp@andrew.cmu.edu>
2020-05-03 21:45:21 -0400

    added feedback form and date limit

--- index.js
+++ index.js
@@ -11,9 +11,14 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 var defaultMoodbarMessage = "Hi newcomers! We wanted to check in on everyone - in this thread, feel free to add any of your comments, suggestions, or questions. This community looks forward to looking at your feedback.";
-var newUserMessage = "\nThese are all the new users in the past month: "
+var newUserMessage = "\nThese are all the new users in the past month: "; 
+var feedbackFormMessage = "\nHere is a link to a feedback form: "; 
 
+var month_map = {0: 31, 1: 28, 2: 31, 3: 30, 4: 31, 5: 30, 6: 31, 7: 31, 8: 30, 9: 31, 10: 30, 11:31};
 
+//TODO: add survey field 
+// TODO - use non default message 
+// idea-> when withinMonth is false, simply call isFirstPull directly, passing the appropriate page number in as well 
 function getAllUsers(client, owner, repo, allUsers, page = 1) {
     return __awaiter(this, void 0, void 0, function* () {
         // Provide console output if we loop for a while.
@@ -38,23 +43,36 @@ function getAllUsers(client, owner, repo, allUsers, page = 1) {
             var user = pull.user.login;
             var prNumber = pull.number; 
             var creationTime = pull.created_at; 
-            console.log("got login: ", user)
-            console.log("creation time", creationTime);
             
             try {
-                // TODO: calculate next withinMonth 
                 // QUESTION: how to determine last month? -- 31 day distance, don't go more than 2 months behind 
+                // DOC: do not include the current day? 
                 
                 //  // figure out if the most recent PR is within the month 
                 var creationDate = new Date(creationTime); 
                 var currDate = new Date(); 
-                //  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse
-                if (creationDate.getYear() != currDate.getYear()) {
-                    var withinMonth = (creationDate.getYear() == currDate.getYear()-1) && currDate.getMonth()==0 && creationDate.getMonth()==11; 
-                } else {
-                    var withinMonth = (currDate.getMonth() - creationDate.getMonth()) <= 1; // check if created_at is less than 1 month from current moment 
+
+                if (currDate.getYear() % 4 == 0) {
+                    month_map[1] = 29; 
                 }
-                console.log("within month:", withinMonth, " , creation date:", creationDate, ", current month: ", currDate.getMonth());
+
+                var withinMonth = false; 
+                //  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse
+                if (false && currDate.getMonth() == creationDate.getMonth() && creationDate.getYear() == currDate.getYear()) {
+                    withinMonth = true;
+                }
+                else if (creationDate.getYear() != currDate.getYear()) {
+                    var prevMonth = (creationDate.getYear() == currDate.getYear()-1) && currDate.getMonth()==0 && creationDate.getMonth()==11; 
+                    
+                } else { // year is the same, month is diff 
+                    var prevMonth = (currDate.getMonth() - creationDate.getMonth()) <= 1; // check if created_at is less than 1 month from current moment 
+                }
+                var dateMinimum = max(month_map[creationTime.getMonth()] - (31 - currDate.getDay())+1, 1);
+                if (!withinMonth) {
+                    withinMonth = prevMonth && creationDate.getDay() >= dateMinimum;
+                }
+
+                console.log("within month:", withinMonth, " , creation date:", creationDate, ", curr date: ", currDate, ", prev month: ", prevMonth, " , date min:", dateMinimum);
             } catch (err){
                 console.log(err);
             }
@@ -122,6 +140,7 @@ function run() {
             const mentors  = core.getInput('mentor-list');
             const repoName = core.getInput('repo-name');
             const repoOwner = core.getInput('repo-owner');
+            const feedbackForm = core.getInput('feedback-form');
 
             // OPTIONALLY: strip whitespace? 
             var mentorArr = mentors.split(',');
@@ -135,18 +154,20 @@ function run() {
                     mentorString += "@";
                     mentorString += mentorArr[i];
                 }
-                console.log("mentor string: ", mentorString); 
                 defaultMoodbarMessage = defaultMoodbarMessage + " Here are the mentors that will be able to answer any questions: " + mentorString; 
             }
 
 
             // get all new users of the past month 
             var newUsers = new Map();
-            // owner = "wwp";
-            // TODO: experiment with owner --> try using Jade's username?
             
             yield getAllUsers(client, repoOwner, repoName, newUsers);
             yield isFirstPull(client, repoOwner, repoName, newUsers); 
+
+            if (newUsers.size == 0) {
+                console.log("There were no new users this month, not creating an issue");
+                return;
+            }
 
             const it = newUsers.keys(); 
             var val = it.next().value;
@@ -164,14 +185,20 @@ function run() {
 
             if (newUserString) {
                 defaultMoodbarMessage = defaultMoodbarMessage + newUserMessage + newUserString; 
-                const context = github.context;    
-                const newIssue = client.issues.create({
+            }
+
+            if (feedbackForm) {
+                defaultMoodbarMessage = feedbackFormMessage + feedbackFormMessage; 
+            }
+
+            const context = github.context;    
+            const newIssue = client.issues.create({
                 ...context.repo,
                 title: 'Moodbar for Current Month',
                 body: defaultMoodbarMessage
             });
 
-            }
+        
 
         } catch(err) {
             console.log(err);
